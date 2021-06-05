ed81ce5292708db4fd6bbdbaf61e7b6f8cce5968
Darya Kiseleva <dkisileva@ltlatn.com>
2021-06-05 21:21:37 +0300

    HW6: pageobject

---
+++ lesson7/conftest.py
@@ -0,0 +1,46 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", "-B", action="store", default="chrome", help="choose your browser")
+    parser.addoption("--url", "-U", action="store", default="https://demo.opencart.com/admin/", help="choose your browser")
+
+
+@pytest.fixture
+def url(request):
+    return request.config.getoption("--url")
+
+
+@pytest.fixture
+def selector():
+    pass
+
+
+@pytest.fixture
+def browser(request, url):
+    browser = request.config.getoption("--browser")
+    if browser == "chrome":
+        options = webdriver.ChromeOptions()
+        options.add_argument('ignore-certificate-errors')
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
+        profile = webdriver.FirefoxProfile()
+        profile.accept_untrusted_certs = True
+        driver = webdriver.Firefox()
+    elif browser == "safari":
+        driver = webdriver.Safari()
+    else:
+        raise Exception(f"{request.param} is not supported!")
+
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+
+    request.addfinalizer(driver.quit)
+
+    def open(path=""):
+        return driver.get(url + path)
+
+    driver.open = open
+    driver.open()
+    return driver
---
+++ lesson7/page_objects/BasePage.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+class BasePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.actions = []
+
+    def submit_login(self):
+        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+    def submit_categories(self):
+        self.driver.find_element(By.CSS_SELECTOR, 'a[href="#collapse1"]').click()
+
+    def submit_products(self):
+        self.driver.find_element(By.LINK_TEXT, 'Products').click()
+
+    def submit_password(self):
+        self.driver.find_element(By.LINK_TEXT, 'Forgotten Password').click()
+
+
+
+    # def _wait_for_visible(self, selector, link_text=None, wait=5):
+    #     return WebDriverWait(self.driver, wait).until(EC.visibility_of(self.__element(selector, index, link_text)))
